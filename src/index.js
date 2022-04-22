5a8df16cc899454cc8cd759cbc5667e97e70c4e9
Alvin Cheng <chengalvin333@gmail.com>
2022-04-22 11:56:13 +1000

    Upload files

---
+++ src/index.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+var inquirer = require("inquirer");
+var signup = require("./signup.js");
+
+var username;
+var password;
+var confirmPassword;
+
+console.log("Welcome to the Chill&chat command line interface.\n");
+console.log("Please enter details below to register a bot.\n");
+
+async function askInput() {
+  await inquirer
+    .prompt({
+      type: "input",
+      name: "username",
+      message: "Please enter username:",
+    })
+    .then(function (data) {
+      username = data.username;
+    });
+
+  await inquirer
+    .prompt({
+      type: "password",
+      name: "password",
+      message: "Please enter your password:",
+    })
+    .then(function (data) {
+      password = data.password;
+    });
+
+  await inquirer
+    .prompt({
+      type: "password",
+      name: "confirmPassword",
+      message: "Please confirm your password:",
+    })
+    .then(function (data) {
+      confirmPassword = data.confirmPassword;
+    });
+}
+
+askInput();
+if (password === confirmPassword) {
+  signup(username, password).then(function (data) {
+    console.log("Successfully created bot.\n");
+    console.log("Bot ID: " + data + "\n");
+  });
+}
---
+++ src/signup.js
@@ -0,0 +1,33 @@
+var uuid = require("uuid");
+var dotenv = require("dotenv");
+/**
+ * This function will send a request to the server to create a new user instance.
+ *
+ * @param {string} username The username of the new user.
+ * @param {string} password The password of the new user.
+ * @returns {Promise<void>}
+ */
+
+dotenv.config();
+
+async function signup(username, password) {
+  var botId = uuid();
+
+  post(process.env.API_URL + "/api/signup?key=" + process.env.API_KEY, {
+    id: botId,
+    username: username,
+    password: password,
+    verified: false,
+    bot: true,
+    blocked: false,
+  })
+    .then(function (_data) {
+      return botId;
+    })
+    .catch(function (err) {
+      console.error(err);
+      process.exit(1);
+    });
+}
+
+module.exports = signup;
