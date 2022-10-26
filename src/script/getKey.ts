8a0306d4230fc7e98577372d657fa982ae30b9d0
Alvin Cheng <chengalvin333@gmail.com>
2022-10-26 20:57:12 +1100

    Update getKey.ts

--- src/script/getKey.ts
+++ src/script/getKey.ts
@@ -4,17 +4,14 @@ import api from "./api";
 /**
  * This is the get key function, this function just fetches the key from the server and sets it as the api key.
  *
- * @param {string} key  The key given by the bot utility
+ * @param {string} key The key given by the bot utility
  */
 
 const getKey = async (key: string): Promise<void> => {
   await api.instance
-    .get(`${api.getKey}?botKey=${key}`)
+    .get(`${api.endpoints.getKey}?botKey=${key}`)
     .then((data: AxiosResponse): void => {
-      api.apiKey = Buffer.from(
-        Buffer.from(data.data, "binary").toString("base64"),
-        "base64"
-      ).toString("ascii");
+      api.apiKey = Buffer.from(data.data, "base64").toString("ascii");
     })
     .catch((err: unknown): void => {
       throw new Error(`API Error: ${err} \n   Error code: CC_ERROR_0318`);
