c98fcc370d97aacc2936ce6315ac2fe6c5f53a28
Alvin Cheng <chengalvin333@gmail.com>
2022-11-03 20:15:33 +1100

    Allow for return data in variables

--- src/runner/commands/set.ts
+++ src/runner/commands/set.ts
@@ -1,4 +1,8 @@
-import { RuntimeInfo } from "./../../utils/index.d";
+import parse from "../../compiler/parse";
+import CompilerError from "../../utils/error";
+import run from "../run";
+import { Command, RuntimeInfo, TreeNode } from "./../../utils/index.d";
+import Commands from "./commands";
 
 /**
  * This the set command, this command will modify a variable or create one if it's not created.
@@ -8,9 +12,40 @@ import { RuntimeInfo } from "./../../utils/index.d";
  */
 
 const set = (parameters: Array<string>, runtimeInfo: RuntimeInfo): void => {
-  runtimeInfo.stack.variableExists(parameters[0])
-    ? runtimeInfo.stack.getVariable(parameters[0]).modify(parameters[1])
-    : runtimeInfo.stack.newVariable(parameters[0], parameters[1]);
+  if (parameters[1][0] !== "(") {
+    runtimeInfo.stack.variableExists(parameters[0])
+      ? runtimeInfo.stack.getVariable(parameters[0]).modify(parameters[1])
+      : runtimeInfo.stack.newVariable(parameters[0], parameters[1]);
+  } else {
+    runtimeInfo.stack.newVariable(parameters[0], "");
+    const statement: Array<TreeNode> = parse(
+      `${parameters[1].slice(1, parameters[1].length - 1)} ${parameters[0]};`
+    );
+
+    let ran: boolean = false;
+    Commands.commands.forEach((command: Command, index: number): void => {
+      if (command.name === statement[0].command) {
+        ran = true;
+        command.method(statement[0].arguments, {
+          line: index + 1,
+          file: process.argv[2],
+          stack: runtimeInfo.stack,
+        });
+      }
+      if (
+        !ran &&
+        command.name !== statement[0].command &&
+        index === Commands.commands.length - 1
+      ) {
+        new CompilerError(
+          `${statement[0].command} is not a valid command, did you mis-spell something??`,
+          process.argv[2],
+          String(index + 1),
+          "error"
+        );
+      }
+    });
+  }
 };
 
 export default set;
