6356047b2915467d069e32e46e37e9edca287ff7
Alvin Cheng <chengalvin333@gmail.com>
2022-11-03 20:15:57 +1100

    Update compare.ts

--- src/runner/commands/compare.ts
+++ src/runner/commands/compare.ts
@@ -1,6 +1,27 @@
+import CompilerError from "../../utils/error";
 import { RuntimeInfo } from "./../../utils/index.d";
 
-const compare = (
-  parameters: Array<string>,
-  runtimeInfo: RuntimeInfo
-): void => {};
+/**
+ * This is the compare command, this command will return the boolean value of wether
+ * parameter 1 is equal to parameter 2.
+ *
+ * @param {Array<string>} parameters The data from the chat-script command.
+ * @param {RuntimeInfo} runtimeInfo The runtime information.
+ */
+
+const compare = (parameters: Array<string>, runtimeInfo: RuntimeInfo): void => {
+  if (runtimeInfo.stack.variableExists(parameters[2])) {
+    runtimeInfo.stack
+      .getVariable(parameters[2])
+      .modify(String(parameters[1] === parameters[0]));
+  } else {
+    new CompilerError(
+      `${parameters[2]} is undefined, did you forget to define it??`,
+      runtimeInfo.file,
+      runtimeInfo.line.toString(),
+      "error"
+    );
+  }
+};
+
+export default compare;
