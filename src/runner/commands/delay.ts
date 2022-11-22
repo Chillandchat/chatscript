beaa9e109d8517b2c7e0401f9a62ece9cdcbaca1
Alvin Cheng <chengalvin333@gmail.com>
2022-11-22 19:02:13 +1100

    Add documentation and support for other time formats

--- src/runner/commands/delay.ts
+++ src/runner/commands/delay.ts
@@ -4,6 +4,11 @@ import CompilerError from "../../utils/error";
 /**
  * This is the delay command, this command will delay/halt the program for however many milliseconds provided in argument 1.
  *
+ * @note
+ * You may also specify 'minutes' or 'seconds' in parameter 2 to infer that it is in a seconds or minutes format.
+ *
+ * @note The default format is milliseconds.
+ *
  * @param {Array<string>} parameters The data from the chat-script command.
  * @param {RuntimeInfo} runtimeInfo The runtime information.
  */
@@ -19,6 +24,12 @@ const delay = (parameters: Array<string>, runtimeInfo: RuntimeInfo): void => {
           "error"
         );
   }
+  if (parameters[1] === "seconds")
+    parameters[0] = String(Number(parameters[0]) * 1000);
+
+  if (parameters[1] === "minutes")
+    parameters[0] = String(Number(parameters[0]) * 6000);
+
   const startTime: number = Date.now();
   while (Date.now() - startTime < Number(parameters[0]));
 };
