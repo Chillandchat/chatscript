8b3b8c8ac5c1dc20d95736a71a5bf446667669cf
Alvin Cheng <chengalvin333@gmail.com>
2022-11-30 20:23:48 +1100

    Format and fix code

--- src/runner/commands/loadstring.ts
+++ src/runner/commands/loadstring.ts
@@ -1,15 +1,20 @@
 import { RuntimeInfo, TreeNode } from "../../utils/index.d";
+import run from "../run";
+import parse from "../../compiler/parse";
 
 /**
-* This is the load string function, this function will parse and run the string in parameter 1
-* 
-* @param {Array<string>} parameters The data from the chat-script command.
-* @param {RuntimeInfo} runtimeInfo The runtime information.
-*/ 
+ * This is the load string function, this function will parse and run the string in parameter 1
+ *
+ * @param {Array<string>} parameters The data from the chat-script command.
+ * @param {RuntimeInfo} runtimeInfo The runtime information.
+ */
 
-const loadString = (parameter: Array<string>, runtimeInfo: RuntimeInfo): void => {
+const loadString = (
+  parameter: Array<string>,
+  _runtimeInfo: RuntimeInfo
+): void => {
   const ast: Array<TreeNode> = parse(parameter[0]);
-  run(ast);
-}
+  run(Buffer.from(JSON.stringify(ast)).toString("base64"));
+};
 
 export default loadString;
